40b178eaa5e209440567ef47e63d47c8f2d2218d
nalauraox <1067519@senacminas.edu.br>
2025-07-31 14:39:23 -0300

    update

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
  
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // importante para conexões com SSL (Render exige isso)
+    }
+  },
+  logging: false,
+});
  
 module.exports = sequelize;
